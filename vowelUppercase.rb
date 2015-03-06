b2565627be4424c3dde7b476d0b4271526460506
boradori <youngsunpaik@gmail.com>
2015-03-06 14:33:37 -0800

    Added error handling method

--- vowelUppercase.rb
+++ vowelUppercase.rb
@@ -1,15 +1,24 @@
-str = "Hello World hello world"
+str1 = "Hello World hello world"
+str2 = 23234
 
 def uppercase(str)
-  str = str.split("")
 
-  str.each do |letter|
-    if letter.include?("a") || letter.include?("e") || letter.include?("i") || letter.include?("o") || letter.include?("u")
-      letter.upcase!
-    end
-    print letter
+  begin
+    str = str.split("")
+  rescue
+    puts "Could not split string."
   end
 
+  begin
+    str.each do |letter|
+      if letter.include?("a") || letter.include?("e") || letter.include?("i") || letter.include?("o") || letter.include?("u")
+        letter.upcase!
+      end
+    print letter
+    end
+  rescue
+    puts "Could not change vowels to uppercase"
+  end
 end
 
-uppercase(str)
+uppercase(str1)
